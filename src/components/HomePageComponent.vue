eaa03d20a76ac0bf92172defd7826479e81ddd3d
kristijan <kristijan@bricksandlogic.com>
2025-01-22 02:02:50 +0000

    image

--- src/components/HomePageComponent.vue
+++ src/components/HomePageComponent.vue
@@ -3,5 +3,10 @@
 </script>
 
 <template>
-  test
+  <div>
+    <div class="w-full h-[10rem] overflow-hidden relative">
+      <img alt="header logo image" src="/images/saint-jerome-split.jpg"
+           class="w-full cover object-contain absolute top-0 bottom-0 right-0 left-0 m-auto"/>
+    </div>
+  </div>
 </template>
