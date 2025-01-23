22d91df8d81b2b7efbdfd4791d7cc0d8467f28ca
kristijan <kristijan@bricksandlogic.com>
2025-01-23 00:43:01 +0000

    container

--- src/components/HomePageComponent.vue
+++ src/components/HomePageComponent.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 
+import FlightsComponent from "@/components/Flights/FlightsComponent.vue";
 </script>
 
 <template>
@@ -7,7 +8,11 @@
     <div class="w-full h-[12rem] overflow-hidden relative">
       <img alt="header logo image" src="/images/saint-jerome-split.jpg"
            class="w-full object-contain absolute top-0 bottom-0 right-0 left-0 m-auto"/>
-      <div class="absolute bottom-0 left-0 right-0 h-[3rem] bg-gradient-to-t from-[#FFFFFF] to-[#FFFFFF00]" />
+      <div
+        class="absolute bottom-0 left-0 right-0 h-[3rem] bg-gradient-to-t from-[#FFFFFF] to-[#FFFFFF00]"/>
+    </div>
+    <div class="w-full max-w-[24rem] m-auto">
+      <FlightsComponent/>
     </div>
   </div>
 </template>
