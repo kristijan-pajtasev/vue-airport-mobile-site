36df727dcb7c1ecf55c633dc23afb7c72eee8b1c
kristijan <kristijan@bricksandlogic.com>
2025-01-23 16:58:36 +0000

    flights in list

--- src/components/Flights/FlightsComponent.vue
+++ src/components/Flights/FlightsComponent.vue
@@ -39,36 +39,48 @@ const flights: Ref<Array<FlightsType>> = ref([
       Arrivals
     </button>
   </div>
+  <div v-for="flight in flights" class="border border-[#cccccc] border-t-0">
+    <div class="flex border-b border-b-[#cccccc]">
+      <div class="w-[4rem] p-[0.5rem]">{{ flight.time }}</div>
+      <div class="flex-[1]">
+        <div>{{ flight.destination }}</div>
+        <div>{{ flight.flightNumber }}</div>
+      </div>
+      <div class="w-[2rem] flex flex-row items-center">
+        {{ ">" }}
+      </div>
+    </div>
+  </div>
   <div class="border border-[#cccccc] border-t-0">
-    <div class="flex border-b border-b-[#cccccc]">
-      <div class="w-[4rem] p-[0.5rem]">18:55</div>
-      <div class="flex-[1]">
-        <div>Barcelona</div>
-        <div>UO1234</div>
-      </div>
-      <div class="w-[2rem] flex flex-row items-center">
-        {{ ">" }}
-      </div>
-    </div>
-    <div class="flex border-b border-b-[#cccccc]">
-      <div class="w-[4rem] p-[0.5rem]">18:55</div>
-      <div class="flex-[1]">
-        <div>Barcelona</div>
-        <div>UO1234</div>
-      </div>
-      <div class="w-[2rem] flex flex-row items-center">
-        {{ ">" }}
-      </div>
-    </div>
-    <div class="flex ">
-      <div class="w-[4rem] p-[0.5rem]">18:55</div>
-      <div class="flex-[1]">
-        <div>Barcelona</div>
-        <div>UO1234</div>
-      </div>
-      <div class="w-[2rem] flex flex-row items-center">
-        {{ ">" }}
-      </div>
-    </div>
+<!--    <div class="flex border-b border-b-[#cccccc]">-->
+<!--      <div class="w-[4rem] p-[0.5rem]">18:55</div>-->
+<!--      <div class="flex-[1]">-->
+<!--        <div>Barcelona</div>-->
+<!--        <div>UO1234</div>-->
+<!--      </div>-->
+<!--      <div class="w-[2rem] flex flex-row items-center">-->
+<!--        {{ ">" }}-->
+<!--      </div>-->
+<!--    </div>-->
+<!--    <div class="flex border-b border-b-[#cccccc]">-->
+<!--      <div class="w-[4rem] p-[0.5rem]">18:55</div>-->
+<!--      <div class="flex-[1]">-->
+<!--        <div>Barcelona</div>-->
+<!--        <div>UO1234</div>-->
+<!--      </div>-->
+<!--      <div class="w-[2rem] flex flex-row items-center">-->
+<!--        {{ ">" }}-->
+<!--      </div>-->
+<!--    </div>-->
+<!--    <div class="flex ">-->
+<!--      <div class="w-[4rem] p-[0.5rem]">18:55</div>-->
+<!--      <div class="flex-[1]">-->
+<!--        <div>Barcelona</div>-->
+<!--        <div>UO1234</div>-->
+<!--      </div>-->
+<!--      <div class="w-[2rem] flex flex-row items-center">-->
+<!--        {{ ">" }}-->
+<!--      </div>-->
+<!--    </div>-->
   </div>
 </template>
