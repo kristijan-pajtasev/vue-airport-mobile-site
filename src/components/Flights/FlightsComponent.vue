adafed36938d21793273e2f56c1a9a95836ea675
kristijan <kristijan@bricksandlogic.com>
2025-01-25 14:24:56 +0000

    basic box styling

--- src/components/Flights/FlightsComponent.vue
+++ src/components/Flights/FlightsComponent.vue
@@ -56,7 +56,7 @@ const flights: Ref<Array<FlightsType>> = ref([
     Show more flights
   </div>
   <div class="grid grid-cols-2 gap-[1rem] mt-[1rem]">
-    <div class="aspect-[1] border border-[#ccc]">
+    <div class="aspect-[1] border border-[#ccc] rounded-lg">
       <div class="w-full h-full  flex flex-row justify-center items-center">
         <div>
           <div class="m-auto mb-[0.5rem] w-[6rem] h-[6rem] aspect-[1] bg-[#AAAAAA]">Icon</div>
@@ -66,8 +66,36 @@ const flights: Ref<Array<FlightsType>> = ref([
         </div>
       </div>
     </div>
-    <div class="aspect-[1] border border-[#ccc]"></div>
-    <div class="aspect-[1] border border-[#ccc]"></div>
-    <div class="aspect-[1] border border-[#ccc]"></div>
+    <div class="aspect-[1] border border-[#ccc] rounded-lg">
+      <div class="w-full h-full  flex flex-row justify-center items-center">
+        <div>
+          <div class="m-auto mb-[0.5rem] w-[6rem] h-[6rem] aspect-[1] bg-[#AAAAAA]">Icon</div>
+          <div class="text-center">Check In
+            <!--      Getting here, Parking, Contact-->
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="aspect-[1] border border-[#ccc] rounded-lg">
+      <div class="w-full h-full  flex flex-row justify-center items-center">
+        <div>
+          <div class="m-auto mb-[0.5rem] w-[6rem] h-[6rem] aspect-[1] bg-[#AAAAAA]">Icon</div>
+          <div class="text-center">Check In
+            <!--      Getting here, Parking, Contact-->
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="aspect-[1] border border-[#ccc] rounded-lg">
+      <div class="w-full h-full  flex flex-row justify-center items-center">
+        <div>
+          <div class="m-auto mb-[0.5rem] w-[6rem] h-[6rem] aspect-[1] bg-[#AAAAAA]">Icon</div>
+          <div class="text-center">Check In
+            <!--      Getting here, Parking, Contact-->
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
