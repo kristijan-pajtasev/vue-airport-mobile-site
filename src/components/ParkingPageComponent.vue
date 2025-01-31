f8794e8b667324725b14294758e1f21d897a08de
kristijan <kristijan@bricksandlogic.com>
2025-01-31 18:36:49 +0000

    moved content

---
+++ src/components/ParkingPageComponent.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="p-[1rem]">
+    <h2>PARKING PRICE LIST for passengers and visitors</h2>
+    <h3>P-2 South Parking</h3>
+    <table>
+      <thead>
+      <tr>
+        <th>Service</th>
+        <th>Price (EUR)</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>Car</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>up to 1 hour</td>
+        <td>1.5</td>
+      </tr>
+      <tr>
+        <td>up to 2 hour</td>
+        <td>3</td>
+      </tr>
+      <tr>
+        <td>up to 3 hour</td>
+        <td>4.5</td>
+      </tr>
+      <tr>
+        <td>up to 4 hour</td>
+        <td>6</td>
+      </tr>
+      <tr>
+        <td>every following hour</td>
+        <td>0.5</td>
+      </tr>
+      <tr>
+        <td>Bus</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>up to 1 hour</td>
+        <td>5.00</td>
+      </tr>
+      <tr>
+        <td>every following hour</td>
+        <td>5.00</td>
+      </tr>
+      </tbody>
+    </table>
+
+    <h3>P-1 North Parking</h3>
+    <table>
+      <thead>
+      <tr>
+        <th>Service</th>
+        <th>Price (EUR)</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>Car</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>up to 1 hour</td>
+        <td>5.00</td>
+      </tr>
+      <tr>
+        <td>every following hour</td>
+        <td>5.00</td>
+      </tr>
+      <tr>
+        <td>Van</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>up to 1 hour</td>
+        <td>5.00</td>
+      </tr>
+      <tr>
+        <td>every following hour</td>
+        <td>5.00</td>
+      </tr>
+      <tr>
+        <td>Bus</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>up to 1 hour</td>
+        <td>5.00</td>
+      </tr>
+      <tr>
+        <td>every following hour</td>
+        <td>5.00</td>
+      </tr>
+      </tbody>
+    </table>
+
+    <h2>Remark</h2>
+    <div>
+      <p>Please, leave the parking within 15 minutes after payment.</p>
+
+
+      <p>VAT is included in the above prices.</p>
+
+
+      <p>Split Airport does not take any responsibility for possible loss or damage of vehicles or
+        objects from them.</p>
+
+
+      <p>By taking parking card, user is accepting all terms and conditions published on price list
+        available at cashiers and Split Airport web page.</p>
+    </div>
+    <h2>PARKING MAP</h2>
+    <div>On the first picture is a whole Split Airport parking map with important elements and on
+      the
+      last two pictures are larger maps of South (P-2) and North Parking (P-1).
+    </div>
+    <h3>Split Airport Parking Map</h3>
+    <div>Picture placeholder</div>
+
+    <h3>South Parking (P-2)</h3>
+    <div>Picture placeholder</div>
+
+    <h3>North Parking (P-1, with permit)</h3>
+    <div>Picture placeholder</div>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
---
+++ src/views/ParkingView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ParkingPageComponent from '@/components/ParkingPageComponent.vue';
+</script>
+
+<template>
+  <main>
+    <ParkingPageComponent/>
+  </main>
+</template>
