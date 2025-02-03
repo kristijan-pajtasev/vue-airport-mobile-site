b0717100fe5a160215277db7db1d66893de90020
kristijan <kristijan@bricksandlogic.com>
2025-02-03 23:50:01 +0000

    flight details view

--- src/router/index.ts
+++ src/router/index.ts
@@ -33,6 +33,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/GettingHereView.vue'),
     },
+    {
+      path: '/flight/:id',
+      name: 'flight-details',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/FlightDetailsView.vue'),
+    },
   ],
 })
 
---
+++ src/views/FlightDetailsView.vue
@@ -0,0 +1,3 @@
+<template>
+  Flight details view
+</template>
