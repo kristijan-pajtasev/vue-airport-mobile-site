bc44dd9af3545ecca7a696ea88b45e697ee37c98
kristijan <kristijan.pajtasev@hotmail.com>
2025-02-05 23:30:53 +0000

    flight initial store

---
+++ src/store/FlightModule.ts
@@ -0,0 +1,36 @@
+const FlightModule = {
+  namespaced: true,
+  state: {
+    isLoading: false,
+    flight: undefined
+  },
+  getters: {
+    isLoading(state) {
+      return state.isLoading
+    },
+    flight(state) {
+      return state.flight
+    },
+  },
+  mutations: {
+    startLoading(state) {
+      state.isLoading = true
+    },
+    stopLoading(state) {
+      state.isLoading = false
+    },
+    setFlight(state, flightData) {
+      state.flight = flightData
+    }
+  },
+  actions: {
+    getFlight({commit}) {
+      commit("startLoading")
+      setTimeout(() => {
+        commit("stopLoading")
+      }, 1500)
+    }
+  }
+}
+
+export default FlightModule;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import {createStore} from 'vuex'
 import FlightsModule from "@/store/FlightsModule.ts";
+import FlightModule from "@/store/FlightModule.ts";
 
 
 const store = createStore({
   modules: {
-    FlightsModule
+    FlightsModule,
+    FlightModule
   }
 });
 
