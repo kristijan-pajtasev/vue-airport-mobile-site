7844b6fadbc399601fe49098d9974fcf5ecce26a
kristijan <kristijan.pajtasev@hotmail.com>
2025-02-05 01:15:21 +0000

    loading flag and flights as computed

--- src/components/Flights/FlightsComponent.vue
+++ src/components/Flights/FlightsComponent.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import {Ref, ref} from "vue";
-import IconCheckIn from "@/components/icons/IconCheckIn.vue";
-import IconBus from "@/components/icons/IconBus.vue";
-import IconParking from "@/components/icons/IconParking.vue";
-import IconContact from "@/components/icons/IconContact.vue";
+import {Ref, ref, computed} from "vue";
 import HomePageTiles from "@/components/HomePageTiles.vue";
 import IconArrivals from "@/components/icons/IconArrivals.vue";
 import IconDepartures from "@/components/icons/IconDepartures.vue";
 import IconArrowRight from "@/components/icons/IconArrowRight.vue";
+import { useStore } from 'vuex'
 
+const store = useStore();
+console.log("useStore: ", store, store.state.count)
+store.dispatch("getFlights")
 interface FlightsType {
   destination: string;
   flightNumber: string;
@@ -16,40 +16,31 @@ interface FlightsType {
   id: number
 }
 
-const flights: Ref<Array<FlightsType>> = ref([
-  {
-    destination: "Barcelona",
-    flightNumber: "UO1234",
-    time: "18:55",
-    id: 1
-  },
-  {
-    destination: "Milan",
-    flightNumber: "UO1235",
-    time: "18:55",
-    id: 2
-  },
-  {
-    destination: "Frankfurt",
-    flightNumber: "UO1236",
-    time: "18:55",
-    id: 3
-  }
-])
+// const flights: Ref<Array<FlightsType>> = ref(store.getters.getFlights)
+const flights = computed(() => {
+  return store.getters.getFlights
+})
+const isLoading = computed(() => {
+  return store.getters.isLoading
+})
+// console.log("other", otherFlights)
 </script>
 
 <template>
   <div class="flex flex-row border border-[#cccccc]">
-    <button type="button" class="flex-[1] flex flex-row gap-[0.5rem] items-center justify-center text-center p-[0.5rem] border-r border-r-[#cccccc]">
+    <button type="button"
+            class="flex-[1] flex flex-row gap-[0.5rem] items-center justify-center text-center p-[0.5rem] border-r border-r-[#cccccc]">
       <span class="inline-block h-[1.5rem]"><IconArrivals/></span>
       Departures
     </button>
-    <button type="button" class="flex-[1] flex flex-row gap-[0.5rem] items-center justify-center text-center p-[0.5rem]">
+    <button type="button"
+            class="flex-[1] flex flex-row gap-[0.5rem] items-center justify-center text-center p-[0.5rem]">
       <span class="inline-block h-[1.5rem]"><IconDepartures/></span>
       Arrivals
     </button>
   </div>
-  <div v-for="(flight, index) in flights" :key="flight.id"
+  <div v-if="isLoading" class="text-center p-[1rem] border border-[#cccccc] border-t-0">Loading data ...</div>
+  <div v-if="!isLoading" v-for="(flight, index) in flights" :key="flight.id"
        class="border border-[#cccccc] border-t-0">
     <div class="flex">
       <div class="w-[4rem] p-[0.5rem]">{{ flight.time }}</div>
@@ -58,7 +49,9 @@ const flights: Ref<Array<FlightsType>> = ref([
         <div>{{ flight.flightNumber }}</div>
       </div>
       <div class="w-[2rem] flex flex-row items-center">
-        <div class="h-[1rem]"><a :href="`/flight/${flight.id}`"><IconArrowRight /></a></div>
+        <div class="h-[1rem]"><a :href="`/flight/${flight.id}`">
+          <IconArrowRight/>
+        </a></div>
       </div>
     </div>
   </div>
---
+++ src/store/index.ts
@@ -0,0 +1,74 @@
+import {createStore} from 'vuex'
+
+const store = createStore({
+  state() {
+    return {
+      count: 0,
+      flights: [],
+      isLoading: true
+    }
+  },
+  getters: {
+    getFlights(state) {
+      return state.flights
+    },
+    isLoading(state) {
+      return state.isLoading
+    }
+  },
+  mutations: {
+    increment(state) {
+      state.count++
+    },
+    startLoading(state) {
+      state.isLoading = true
+    },
+    stopLoading(state) {
+      state.isLoading = false
+    },
+    setFlights(state, data) {
+      state.flights = data;
+    },
+  },
+  methods: {
+    increment() {
+      this.$store.commit('increment')
+      console.log(this.$store.state.count)
+    },
+    startLoading() {
+      this.$store.commit('startLoading')
+    },
+    stopLoading() {
+      this.$store.commit('stopLoading')
+    }
+  },
+  actions: {
+    getFlights({commit}) {
+      commit("startLoading")
+      setTimeout(() => {
+        commit("setFlights", [
+          {
+            destination: "Barcelona",
+            flightNumber: "UO1234",
+            time: "18:55",
+            id: 1
+          },
+          {
+            destination: "Milan",
+            flightNumber: "UO1235",
+            time: "18:55",
+            id: 2
+          },
+          {
+            destination: "Frankfurt",
+            flightNumber: "UO1236",
+            time: "18:55",
+            id: 3
+          }])
+        commit("stopLoading")
+      }, 1500)
+    }
+  }
+});
+
+export default store
