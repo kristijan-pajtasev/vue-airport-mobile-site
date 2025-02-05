00b297b0480fd49ce3d770db1eb16b41f63beea4
kristijan <kristijan.pajtasev@hotmail.com>
2025-02-05 22:13:34 +0000

    flights toggle

--- src/components/Flights/FlightsComponent.vue
+++ src/components/Flights/FlightsComponent.vue
@@ -4,11 +4,12 @@ import HomePageTiles from "@/components/HomePageTiles.vue";
 import IconArrivals from "@/components/icons/IconArrivals.vue";
 import IconDepartures from "@/components/icons/IconDepartures.vue";
 import IconArrowRight from "@/components/icons/IconArrowRight.vue";
-import { useStore } from 'vuex'
+import {useStore} from 'vuex'
 
 const store = useStore();
 console.log("useStore: ", store, store.state.count)
 store.dispatch("getFlights")
+
 interface FlightsType {
   destination: string;
   flightNumber: string;
@@ -23,23 +24,36 @@ const flights = computed(() => {
 const isLoading = computed(() => {
   return store.getters.isLoading
 })
-// console.log("other", otherFlights)
+
+const flightDisplayType = ref("ARRIVALS")
+
+const toggleFlights = (type: string) => {
+  if (flightDisplayType.value !== type)
+    flightDisplayType.value = type;
+}
+
 </script>
 
 <template>
   <div class="flex flex-row border border-[#cccccc]">
     <button type="button"
-            class="flex-[1] flex flex-row gap-[0.5rem] items-center justify-center text-center p-[0.5rem] border-r border-r-[#cccccc]">
+            @click="toggleFlights('ARRIVALS')"
+            class="flex-[1] flex flex-row gap-[0.5rem] items-center justify-center text-center p-[0.5rem] border-r border-r-[#cccccc]"
+            :class="flightDisplayType === 'ARRIVALS' ? '' : 'bg-[#cccccc]'">
       <span class="inline-block h-[1.5rem]"><IconArrivals/></span>
-      Departures
-    </button>
-    <button type="button"
-            class="flex-[1] flex flex-row gap-[0.5rem] items-center justify-center text-center p-[0.5rem]">
-      <span class="inline-block h-[1.5rem]"><IconDepartures/></span>
       Arrivals
     </button>
+    <button type="button"
+            @click="toggleFlights('DEPARTURES')"
+            class="flex-[1] flex flex-row gap-[0.5rem] items-center justify-center text-center p-[0.5rem]"
+            :class="flightDisplayType === 'DEPARTURES' ? '' : 'bg-[#cccccc]'">
+      <span class="inline-block h-[1.5rem]"><IconDepartures/></span>
+      Departures
+    </button>
   </div>
-  <div v-if="isLoading" class="text-center p-[1rem] border border-[#cccccc] border-t-0">Loading data ...</div>
+  <div v-if="isLoading" class="text-center p-[1rem] border border-[#cccccc] border-t-0">Loading data
+    ...
+  </div>
   <div v-if="!isLoading" v-for="(flight, index) in flights" :key="flight.id"
        class="border border-[#cccccc] border-t-0">
     <div class="flex">
